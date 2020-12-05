995a4dff6f319e4e7c5ba38b7df626b719358878
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-12-05 23:56:57 +0100

    Enhancing Instance counter class

--- Source/InstanceCounter.h
+++ Source/InstanceCounter.h
@@ -1,15 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <atomic>
 
 template<typename T>
 class Counter
 {
 public:
-	Counter() :
-		m_Id(++m_Count)
-	{}
-
 	Counter& operator=(const Counter& other)
 	{
 		return *this;
@@ -20,18 +17,31 @@ public:
 		return m_Id;
 	}
 
-	static uint32_t Count()
-	{
-		return m_Count;
-	}
+	static uint32_t CreatedObjects() { return m_CreatedObjects; }
+
+	static uint32_t LiveObjects() { return m_LiveObjects; }
 
 protected:
-	~Counter() = default;
+	Counter() :
+		m_Id(++m_CreatedObjects)
+	{
+		++m_LiveObjects;
+	}
+
+	~Counter()
+	{
+		--m_LiveObjects;
+	}
 
 private:
 	uint32_t m_Id;
-	static uint32_t m_Count;
+
+	static std::atomic_uint m_CreatedObjects;
+	static std::atomic_uint m_LiveObjects;
 };
 
 template<typename T>
-uint32_t Counter<T>::m_Count = 0;
+std::atomic_uint Counter<T>::m_CreatedObjects = 0;
+
+template<typename T>
+std::atomic_uint Counter<T>::m_LiveObjects = 0;
