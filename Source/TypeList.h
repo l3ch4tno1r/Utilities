89fcf3c980df878eb0a5c845a1672076e7d547d6
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-03-14 23:30:45 +0100

    Add TypeList.h

---
+++ Source/TypeList.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace LCN
+{
+	//////////////////
+	//-- TypeList --//
+	//////////////////
+
+	template<typename _Head, typename ... _Tail>
+	struct TypeList
+	{
+		using Head = _Head;
+		using Tail = TypeList<_Tail...>;
+
+		static constexpr size_t Count() { return Tail::Count() + 1; }
+	};
+
+	template<typename Single>
+	struct TypeList<Single>
+	{
+		using Head = Single;
+		using Tail = void;
+
+		static constexpr size_t Count() { return 1; }
+	};
+
+	/////////////////
+	//-- Extract --//
+	/////////////////
+
+	template <size_t Idx, typename typelist>
+	struct Extract
+	{
+		using Result = typename Extract<Idx - 1, typename typelist::Tail>::Result;
+	};
+
+	template <typename typelist>
+	struct Extract <0, typelist>
+	{
+		using Result = typename typelist::Head;
+	};
+}
