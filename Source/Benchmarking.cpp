270c06805bb6c143f6ec73cf3d6f6d7d38d18997
l3ch4tno1r <arnaud.granier34@gmail.com>
2021-03-15 01:26:37 +0100

    Commit

--- Source/Benchmarking.cpp
+++ Source/Benchmarking.cpp
@@ -1,24 +1,21 @@
 #include "Benchmarking.h"
 
-namespace LCN
+namespace LCN::Benchmark
 {
-	namespace Benchmark
+	SessionTimer::SessionTimer(Session& session) :
+		m_Session(session),
+		m_Start(std::chrono::high_resolution_clock::now())
+	{}
+
+	SessionTimer::~SessionTimer()
 	{
-		SessionTimer::SessionTimer(Session& session) :
-			m_Session(session),
-			m_Start(std::chrono::high_resolution_clock::now())
-		{}
+		auto end = std::chrono::high_resolution_clock::now();
 
-		SessionTimer::~SessionTimer()
-		{
-			auto end = std::chrono::high_resolution_clock::now();
+		m_Session.m_SessionDurationNanos = std::chrono::duration_cast<std::chrono::nanoseconds>(end - m_Start).count();
+	}
 
-			m_Session.m_SessionDurationNanos = std::chrono::duration_cast<std::chrono::nanoseconds>(end - m_Start).count();
-		}
-
-		long long Session::Duration(DurationUnit unit)
-		{
-			return m_SessionDurationNanos / unit;
-		}
+	long long Session::Duration(DurationUnit unit)
+	{
+		return m_SessionDurationNanos / unit;
 	}
 }
--- Source/Benchmarking.h
+++ Source/Benchmarking.h
@@ -3,65 +3,62 @@
 #include <future>
 #include <chrono>
 
-namespace LCN
+namespace LCN::Benchmark
 {
-	namespace Benchmark
+	enum DurationUnit
 	{
-		enum DurationUnit
+		Seconds      = 1000000000,
+		Milliseconds = 1000000,
+		Microseconds = 1000,
+		Nanoseconds  = 1
+	};
+
+	class Session
+	{
+	public:
+		long long Duration(DurationUnit unit);
+
+	private:
+		long long m_SessionDurationNanos;
+
+		friend class SessionTimer;
+	};
+
+	class SessionTimer
+	{
+	public:
+		SessionTimer(Session& session);
+
+		~SessionTimer();
+
+	private:
+		Session& m_Session;
+
+		std::chrono::steady_clock::time_point m_Start;
+	};
+
+	template<class Function, typename ...Args>
+	Session TimePerformance(size_t iterations, Function f, Args&&... args)
+	{
+		Session session;
+
 		{
-			Seconds      = 1000000000,
-			Milliseconds = 1000000,
-			Microseconds = 1000,
-			Nanoseconds  = 1
-		};
-
-		class Session
-		{
-		public:
-			long long Duration(DurationUnit unit);
-
-		private:
-			long long m_SessionDurationNanos;
-
-			friend class SessionTimer;
-		};
-
-		class SessionTimer
-		{
-		public:
-			SessionTimer(Session& session);
-
-			~SessionTimer();
-
-		private:
-			Session& m_Session;
-
-			std::chrono::steady_clock::time_point m_Start;
-		};
-
-		template<class Function, typename ...Args>
-		Session TimePerformance(size_t iterations, Function f, Args&&... args)
-		{
-			Session session;
-
-			{
-				SessionTimer timer(session);
+			SessionTimer timer(session);
 	
-				for (size_t i = 0; i < iterations; ++i)
-					f(std::forward<Args>(args)...);
-			}
-
-			return session;
+			for (size_t i = 0; i < iterations; ++i)
+				f(std::forward<Args>(args)...);
 		}
 
-		template<class Function, typename ...Args>
-		std::future<Session> TimePerformanceAsync(size_t iterations, Function f, Args&&... args)
-		{
-			return std::async(
-				std::launch::async,
-				TimePerformance<Function, Args...>, iterations,
-				f,
-				std::forward<Args>(args)...);
-		}
+		return session;
+	}
+
+	template<class Function, typename ...Args>
+	std::future<Session> TimePerformanceAsync(size_t iterations, Function f, Args&&... args)
+	{
+		return std::async(
+			std::launch::async,
+			TimePerformance<Function, Args...>, iterations,
+			f,
+			std::forward<Args>(args)...);
 	}
 }
