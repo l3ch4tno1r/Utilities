2b35114a4f0aa5723629d442c75238c6ad3b1a4d
l3ch4tno1r <arnaud.granier34@gmail.com>
2023-04-23 17:03:41 +0200

    Refactoring Sychrononized

--- include/Utilities/Synchronized.h
+++ include/Utilities/Synchronized.h
@@ -3,41 +3,85 @@
 #include <type_traits>
 #include <mutex>
 
-template<typename _Type>
-class Synchronized
+namespace LCN::Utilities
 {
-public:
-	using ValType = std::remove_reference<_Type>::type;
-
-	class SyncLock : public std::lock_guard<std::mutex>
+	/**
+	 * @class Locker<_ThreadSafe>
+	 * @brief Scope based locker class that holds a mutex.
+	*/
+	template<typename _ThreadSafe>
+	class Locker
+		: public std::scoped_lock<typename _ThreadSafe::MtxType>
 	{
 	public:
-		using BaseType = std::lock_guard<std::mutex>;
+		using MtxType  = typename _ThreadSafe::MtxType;
+		using BaseType = std::scoped_lock<MtxType>;
+		using ValType  = typename _ThreadSafe::ValType;
+	
+	public:
+		ValType&
+		operator*() const
+		{
+			return m_target;
+		}
 
-		ValType& operator*() { return m_Ref; }
-
-		ValType* operator->() { return &m_Ref; }
+		ValType*
+		operator->() const
+		{
+			return &m_target;
+		}
 
 	private:
-		SyncLock(ValType& ref, std::mutex& mut) :
-			m_Ref{ ref },
-			BaseType{ mut }
+		Locker(ValType& target, MtxType& mutex)
+			: m_target{ target }
+			, BaseType{ mutex }
 		{}
 
-	private:
-		ValType& m_Ref;
+		friend _ThreadSafe;
 
-		friend Synchronized;
+	private:
+		ValType& m_target;
 	};
 
-	template<typename ... Args>
-	Synchronized(Args&& ... args) :
-		m_Data(std::forward<Args>(args)...)
-	{}
+	/**
+	 * @class ThreadSafe<_DataType, _MtxType>
+	 * @brief Class that holds data in a multi thread environnement.
+	*/
+	template<
+		typename _DataType,
+		typename _MtxType = std::mutex>
+	class ThreadSafe
+	{
+	public:
+		using ThisType = ThreadSafe<_DataType>;
+		using ValType  = std::remove_reference<_DataType>::type;
+		using MtxType  = _MtxType;
+		using LockType = Locker<ThisType>;
 
-	SyncLock Lock() { return { m_Data, m_Mut }; }
+		template<typename ... Args>
+		ThreadSafe(Args&& ... args) :
+			m_Data(std::forward<Args>(args)...)
+		{}
 
-private:
-	_Type m_Data;
-	std::mutex m_Mut;
-};
+		LockType Lock() { return { m_Data, m_Mut }; }
+
+	private:
+		_DataType m_Data;
+		_MtxType  m_Mut;
+	};
+
+	/**
+	 * @brief Convenience function to create a wrapping ThreadSafe object.
+	 * @param args Variadic arguments to be passed to the ThreadSafe constructor.
+	*/
+	template<
+		typename _DataType,
+		typename _MtxType = std::mutex,
+		typename ... _Args>
+	ThreadSafe<_DataType, _MtxType>
+	MakeThreadSafe(
+		_Args&& ... args)
+	{
+		return { std::forward<_Args>(args)... };
+	}
+}
